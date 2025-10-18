9b2d679a4cbcd9c04cd39bad816eaa9a80ba6092
EmperorSARATH <ukm7773@gmail.com>
2025-10-18 12:58:32 +0530

    made changes to the h1 tag in main page heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,52 @@
 "use client"; // Ensure client-side rendering
 import { useRef, useState, useEffect } from "react";
 import HomeUserSelection from "./components/homeUserSelection";
-import "./page.css"; 
+import "./page.css";
 
 export default function SmoothScrollPage() {
-  const nextSectionRef = useRef<HTMLDivElement>(null);
+    const nextSectionRef = useRef<HTMLDivElement>(null);
 
-  const handleArrowClick = () => {
-    nextSectionRef.current?.scrollIntoView({ behavior: "smooth" });
-  };
+    const handleArrowClick = () => {
+        nextSectionRef.current?.scrollIntoView({ behavior: "smooth" });
+    };
 
-  const phrases = [
-    "Explore Opportunities",
-    "Welcome to Job Portal",
-    "Collaborate with Brilliant Minds",
-  ];
+    const phrases = [
+        "Explore Opportunities",
+        "Welcome to Job Portal",
+        "Collaborate with Brilliant Minds",
+    ];
 
-  const [fade, setFade] = useState(true);
+    const [fade, setFade] = useState(true);
 
-  const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prev) => (prev + 1) % phrases.length);
-    }, 2000);
-    return () => clearInterval(interval);
-  }, []);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex((prev) => (prev + 1) % phrases.length);
+        }, 2000);
+        return () => clearInterval(interval);
+    }, []);
 
-  return (
-    <>
-      <div className="flex items-center justify-center flex-col h-screen">
-        <div className="current-page-content">
-          <h1
-            className="text-center text-5xl custom-h1"
-            style={{ fontFamily: "'Lobster', cursive" }}
-          >
-            <h1
-              className={`custom-h1 ${fade ? "fade-in" : "fade-out"}`}
-            >
-              {phrases[currentIndex]}
-            </h1>
+    return (
+        <>
+            <div className="flex items-center justify-center flex-col h-screen">
+                <div className="current-page-content">
 
+                    <h1
+                        className={`"text-center text-5xl custom-h1" custom-h1 ${fade ? "fade-in" : "fade-out"}`}
+                        style={{ fontFamily: "'Lobster', cursive" }}
 
-          </h1>
+                    >
+                        {phrases[currentIndex]}
+                    </h1>
+                    <div className="arrow-container" onClick={handleArrowClick}>
+                        <span className="arrow text-2xl">{"▼"}</span>
+                    </div>
+                </div>
+            </div>
 
-          <div className="arrow-container" onClick={handleArrowClick}>
-            <span className="arrow text-2xl">{"▼"}</span>
-          </div>
-        </div>
-      </div>
-
-      {/* New Section */}
-      <HomeUserSelection ref={nextSectionRef} />
-    </>
-  );
+            {/* New Section */}
+            <HomeUserSelection ref={nextSectionRef} />
+        </>
+    );
 }
