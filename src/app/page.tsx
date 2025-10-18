d3707ae446ed9d0adb6c09eedfeb1a9fdf9543e8
anamikaanilkumar <137039559+anamikaanilkumar@users.noreply.github.com>
2025-10-18 11:38:44 +0530

    made changes to animation in landing page heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
-
 "use client"; // Ensure client-side rendering
-import { useRef } from "react";
-import HomeUserSelection from './components/homeUserSelection';
+import { useRef, useState, useEffect } from "react";
+import HomeUserSelection from "./components/homeUserSelection";
+import "./page.css"; 
+
 export default function SmoothScrollPage() {
   const nextSectionRef = useRef<HTMLDivElement>(null);
 
   const handleArrowClick = () => {
     nextSectionRef.current?.scrollIntoView({ behavior: "smooth" });
   };
+
+  const phrases = [
+    "Explore Opportunities",
+    "Welcome to Job Portal",
+    "Collaborate with Brilliant Minds",
+  ];
+
+  const [fade, setFade] = useState(true);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prev) => (prev + 1) % phrases.length);
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <>
@@ -17,10 +35,17 @@ export default function SmoothScrollPage() {
             className="text-center text-5xl custom-h1"
             style={{ fontFamily: "'Lobster', cursive" }}
           >
-            Welcome to Job-Portal
+            <h1
+              className={`custom-h1 ${fade ? "fade-in" : "fade-out"}`}
+            >
+              {phrases[currentIndex]}
+            </h1>
+
+
           </h1>
+
           <div className="arrow-container" onClick={handleArrowClick}>
-            <span className="arrow text-2xl" >{"▼"}</span>
+            <span className="arrow text-2xl">{"▼"}</span>
           </div>
         </div>
       </div>
@@ -30,4 +55,3 @@ export default function SmoothScrollPage() {
     </>
   );
 }
-
