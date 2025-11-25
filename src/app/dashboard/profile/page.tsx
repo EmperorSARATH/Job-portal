34d8bcecfbbf8d8691396610f7bc9f92f5d11463
EmperorSARATH <ukm7773@gmail.com>
2025-11-25 22:47:16 +0530

    profile page sidebar

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,18 +1,32 @@
 
 
+import './page.css';
 export default function Page() {
-return (
-<div className="flex h-screen w-full">
-{/* Sidebar */}
-<aside className="w-64 bg-gray-100 border-r border-gray-300 p-6">
-<h1 className="text-xl font-semibold mb-4">Profile</h1>
-</aside>
+    return (
+        <div className="flex h-screen w-full">
+            {/* Sidebar */}
+            <aside className="sidebar  bg-gray-100 border-r border-gray-300 p-6">
+                <div className="avatar mb-4">
+                    <img src="/Images/pic0.jpg" alt="avatar" />
+                </div>
+                <h1 className="text-xl font-semibold mb-4 text-black">Profile</h1>
+                <h1 className='text-black mb-4'>-------------------------</h1>
+                <ul className='text-black'>
+                    <li><button>Personal Detail</button></li>
+                    <li><button>Profile Summary</button></li>
+                    <li><button>Skills</button></li>
+                    <li><button>Saved jobs</button></li>
+                    <li><button>Github connect </button></li>
 
 
-{/* Main Content */}
-<main className="flex-1 p-6">
-{/* Empty for now */}
-</main>
-</div>
-);
+                </ul>
+            </aside>
+
+
+            {/* Main Content */}
+            <main className="flex-1 p-6">
+                {/* Empty for now */}
+            </main>
+        </div>
+    );
 }
