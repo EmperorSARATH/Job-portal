a5c03ac8376ae9853f449833ac23b6e199fd784d
EmperorSARATH <ukm7773@gmail.com>
2025-12-05 21:43:44 +0530

    next config change

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Allow production builds to complete even if there are ESLint errors
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Optional: also ignore TypeScript errors if you have any (not needed now)
     ignoreBuildErrors: true,
   },
+  output: 'standalone',   // ← THIS LINE IS REQUIRED FOR .next/standalone
 };
 
 module.exports = nextConfig;
